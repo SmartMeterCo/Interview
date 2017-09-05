c1075fc956e70de993119b86b9e8a025abeb57de
Tom Halson <tom@smartmetercompany.com>
2017-09-05 11:05:35 +0100

    First Commit

---
+++ InterviewQuestions/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Question5
+{
+    public class Animal
+    {
+        public string AnimalType { get; set; }
+        public string AnimalSound;
+
+        public Animal(string animalType)
+        {
+            AnimalType = animalType;
+        }
+        public void MakeSound()
+        {
+            if (AnimalSound == null)
+            {
+                switch (AnimalType)
+                {
+                    case "Cat":
+                        Console.WriteLine("Meow");
+                        AnimalSound = "Meow";
+                        break;
+                    case "Dog":
+                        Console.WriteLine("Woof");
+                        AnimalSound = "Woof";
+                        break;
+                    case "Bird":
+                        Console.WriteLine("Tweet");
+                        AnimalSound = "Tweet";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine(AnimalSound);
+            }
+        }
+    }
+}
