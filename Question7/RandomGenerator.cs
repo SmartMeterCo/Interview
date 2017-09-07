22f8fb2d7c149a00beefcdddf1c1cf2322622261
Tom Halson <tom@smartmetercompany.com>
2017-09-07 11:04:43 +0100

    Did some tidying

---
+++ Question7/RandomGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question7
+{
+    public class RandomGenerator
+    {
+        public int GetRandomNumber()
+        {
+            var random = new Random();
+            return random.Next(0, 2);
+        }
+        
+    }
+}
