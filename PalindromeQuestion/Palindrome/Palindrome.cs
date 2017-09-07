87bb4eacbd052846c31e58cb4673bebf50b9a0c0
James Bourne <james@smartmetercompany.com>
2017-09-07 11:45:16 +0100

    Add skeleton setup for Palindrome question

---
+++ PalindromeQuestion/Palindrome/Palindrome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palindrome
+{
+    public static class Palindrome
+    {
+        public static bool IsPalindrome(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
